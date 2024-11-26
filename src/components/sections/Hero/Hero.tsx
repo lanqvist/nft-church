07498d468a387135de1dbd7ee6134e76d763592c
wypwawii <wypwawii@gmail.com>
2024-11-26 14:21:46 +0300

    Поправлен контейнер и скорректированы брейкпоинты WILDTURKEY-1697

--- src/components/sections/Hero/Hero.tsx
+++ src/components/sections/Hero/Hero.tsx
@@ -5,16 +5,18 @@ import { Section } from '@components/layout/Section';
 import styles from './Hero.module.css';
 
 export const Hero = () => (
-    <Section containerClassName={styles.container}>
-        <div className={styles.infoWrapper}>
-            <div className={styles.info}>
-                <Title order={1} className={styles.title}>
-                    Благотворительный{'\n'}сбор средств
-                </Title>
-                <Text className={styles.text}>Храма преподобного Сергия Радонежского</Text>
-                <Button color="green" variant="filled" className={styles.button}>
-                    Пожертвовать
-                </Button>
+    <Section>
+        <div className={styles.content}>
+            <div className={styles.infoWrapper}>
+                <div className={styles.info}>
+                    <Title order={1} className={styles.title}>
+                        Благотворительный{'\n'}сбор средств
+                    </Title>
+                    <Text className={styles.text}>Храма преподобного Сергия Радонежского</Text>
+                    <Button color="green" variant="filled" className={styles.button}>
+                        Пожертвовать
+                    </Button>
+                </div>
             </div>
         </div>
     </Section>
