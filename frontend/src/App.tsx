529a6f9c1cd8d40fb01f441f6004b734f8c94fc0
Vladislav Galkin <lanqvist69@gmail.com>
2025-05-19 14:36:31 +0300

    yandex metrika

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { useInViewport } from '@mantine/hooks';
+import { YMInitializer } from 'react-yandex-metrika';
 
 import { FloatingButton } from '@components/floating-button';
 import { Certificate } from '@components/sections/Certificate';
@@ -9,17 +10,25 @@ const App = () => {
     const { ref, inViewport } = useInViewport();
 
     return (
-        <MainLayout>
-            <FloatingButton heroInView={inViewport} />
-            <Hero ref={ref} />
-            <About />
-            <Certificate />
-            <WorkStages />
-            <Introduction />
-            <Facts />
-            <Donate />
-            <Contacts />
-        </MainLayout>
+        <>
+            <YMInitializer accounts={[101985849]} options={{
+                webvisor: true,
+                clickmap: true,
+                trackLinks: true,
+                accurateTrackBounce: true
+            }} />
+            <MainLayout>
+                <FloatingButton heroInView={inViewport} />
+                <Hero ref={ref} />
+                <About />
+                <Certificate />
+                <WorkStages />
+                <Introduction />
+                <Facts />
+                <Donate />
+                <Contacts />
+            </MainLayout>
+        </>
     );
 };
 
