4271690d80e21e99f67e44d790365e5a9dc6fe5e
Vladislav Galkin <lanqvist69@gmail.com>
2025-12-23 19:46:21 +0300

    new stage image

--- frontend/src/components/sections/WorkStages/WorkStages.consts.ts
+++ frontend/src/components/sections/WorkStages/WorkStages.consts.ts
@@ -1,6 +1,4 @@
 import stageSlideImg1 from './assets/stageSlide1.jpeg';
-import stageSlideImg10 from './assets/stageSlide10.jpeg';
-import stageSlideImg11 from './assets/stageSlide11.jpeg';
 import stageSlideImg2 from './assets/stageSlide2.jpeg';
 import stageSlideImg3 from './assets/stageSlide3.jpeg';
 import stageSlideImg4 from './assets/stageSlide4.jpeg';
@@ -9,6 +7,14 @@ import stageSlideImg6 from './assets/stageSlide6.jpeg';
 import stageSlideImg7 from './assets/stageSlide7.jpeg';
 import stageSlideImg8 from './assets/stageSlide8.jpeg';
 import stageSlideImg9 from './assets/stageSlide9.jpeg';
+import stageSlideImg10 from './assets/stageSlide10.jpeg';
+import stageSlideImg11 from './assets/stageSlide11.jpeg';
+import stageSlideImg12 from './assets/stageSlide12.png';
+import stageSlideImg13 from './assets/stageSlide13.png';
+import stageSlideImg14 from './assets/stageSlide14.png';
+import stageSlideImg15 from './assets/stageSlide15.png';
+import stageSlideImg16 from './assets/stageSlide16.png';
+import stageSlideImg17 from './assets/stageSlide17.png';
 
 export const CARD_IMAGES = [
     stageSlideImg1,
@@ -22,4 +28,10 @@ export const CARD_IMAGES = [
     stageSlideImg9,
     stageSlideImg10,
     stageSlideImg11,
+    stageSlideImg12,
+    stageSlideImg13,
+    stageSlideImg14,
+    stageSlideImg15,
+    stageSlideImg16,
+    stageSlideImg17,
 ];
