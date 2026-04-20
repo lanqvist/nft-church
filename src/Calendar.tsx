c1e277905e331214f3dee2e2c792b0df2df47567
Vladislav Galkin <lanqvist69@gmail.com>
2026-04-20 16:24:31 +0300

    Vibe Coding: добавить-на

---
+++ src/Calendar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './index.css'
+
+export const Calendar: React.FC = () => {
+  return (
+    <div className="app-container">
+      <div className="app-card">
+        <h1>Calendar</h1>
+        <p>Компонент готов к разработке</p>
+      </div>
+    </div>
+  )
+}
