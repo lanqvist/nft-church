734485bef915d43ec5e7e9ede64ee6566f005ac6
Vladislav Galkin <lanqvist69@gmail.com>
2026-04-21 15:08:42 +0300

    Vibe Coding: сделай-рефаторинг

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+const App: React.FC = () => {
+  return (
+    <div className="app-container">
+      <div className="app-card">
+        <div className="app-header">
+          <h1>сделай-рефаторинг</h1>
+          <p className="subtitle">Сделай рефаторинг кода</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
